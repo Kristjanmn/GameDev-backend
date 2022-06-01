8c800eb6a1f28496eb941eb24be7561faf1a1852
Kristjanmn <kristjanmn@hotmail.com>
2022-06-01 23:35:47 +0300

    Initial

---
+++ src/main/java/io/nqa/gamedev/GamedevApplication.java
@@ -0,0 +1,13 @@
+package io.nqa.gamedev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamedevApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamedevApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/nqa/gamedev/GamedevApplicationTests.java
@@ -0,0 +1,13 @@
+package io.nqa.gamedev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamedevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
