cca716f621e441ec5a099e58314bc86c0e7f94e2
Kristjanmn <kristjanmn@hotmail.com>
2022-06-02 12:51:40 +0300

    Created Project and it's service, repository

---
+++ src/main/java/io/nqa/gamedev/entity/Project.java
@@ -0,0 +1,30 @@
+package io.nqa.gamedev.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "project")
+public class Project {
+
+    @Id
+    private String id;
+
+    private String title;
+
+    @OneToMany
+    private List<Quest> quests;
+
+    @OneToMany
+    private List<Dialog> dialogs;
+
+    @OneToMany
+    private List<Script> scripts;
+}
