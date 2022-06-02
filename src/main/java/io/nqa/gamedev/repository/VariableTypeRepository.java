fca012aa58d8bb04c76be33f8fc1b7da3a39021b
Kristjanmn <kristjanmn@hotmail.com>
2022-06-02 18:47:40 +0300

    Removed VariableType, as it does not have any advantages

--- src/main/java/io/nqa/gamedev/repository/VariableTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package io.nqa.gamedev.repository;
-
-import io.nqa.gamedev.entity.VariableType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VariableTypeRepository extends JpaRepository<VariableType, String> {
-}
