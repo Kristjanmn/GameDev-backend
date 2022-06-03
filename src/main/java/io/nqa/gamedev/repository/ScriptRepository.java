29c93bff3429051572128b3397e553e6f786e065
Kristjanmn <kristjanmn@hotmail.com>
2022-06-03 16:16:32 +0300

    Created ScriptRepository.findByNameEqualsAndGlobalEquals

--- src/main/java/io/nqa/gamedev/repository/ScriptRepository.java
+++ src/main/java/io/nqa/gamedev/repository/ScriptRepository.java
@@ -1,6 +1,7 @@
 package io.nqa.gamedev.repository;
 
 import io.nqa.gamedev.entity.Script;
+import io.nqa.gamedev.entity.ScriptVariable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.Optional;
 @Repository
 public interface ScriptRepository extends JpaRepository<Script, String> {
     Optional<List<Script>> findAllByGlobalIsTrue();
+
+    Optional<Script> findByNameEqualsAndGlobalEquals(String scriptName, boolean isGlobal);
 }
