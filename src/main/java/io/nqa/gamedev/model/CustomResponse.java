e28ac50ce2f307975e579896f1f152488c4a6f18
Kristjanmn <kristjanmn@hotmail.com>
2022-06-06 16:42:17 +0300

    Added alternative constructor for CustomResponse

--- src/main/java/io/nqa/gamedev/model/CustomResponse.java
+++ src/main/java/io/nqa/gamedev/model/CustomResponse.java
@@ -1,5 +1,6 @@
 package io.nqa.gamedev.model;
 
+import io.nqa.gamedev.service.global.GlobalService;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
@@ -9,4 +10,16 @@ public class CustomResponse {
     private boolean success;
     private String message;
     private Object object;
+
+    public CustomResponse(Object object) {
+        if (GlobalService.isNull(object)) {
+            this.success = false;
+            this.message = "failure";
+            this.object = null;
+        } else {
+            this.success = true;
+            this.message = "success";
+            this.object = object;
+        }
+    }
 }
