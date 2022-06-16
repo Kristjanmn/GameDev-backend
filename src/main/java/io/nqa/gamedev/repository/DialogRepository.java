69a1ce417991bc7b8f69abbf0fb98dac8e299f04
Kristjanmn <kristjanmn@hotmail.com>
2022-06-17 02:48:50 +0300

    Added DialogRepository.findByDialogIdEquals

--- src/main/java/io/nqa/gamedev/repository/DialogRepository.java
+++ src/main/java/io/nqa/gamedev/repository/DialogRepository.java
@@ -4,6 +4,10 @@ import io.nqa.gamedev.entity.Dialog;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface DialogRepository extends JpaRepository<Dialog, String> {
+
+    Optional<Dialog> findByDialogIdEquals(String dialogId);
 }
