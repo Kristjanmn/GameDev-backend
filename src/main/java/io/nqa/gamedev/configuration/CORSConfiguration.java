bebbf2a4e08e6de9b6035255639f2991e135f914
Kristjanmn <kristjanmn@hotmail.com>
2022-06-17 02:43:37 +0300

    Created CORSConfiguration

---
+++ src/main/java/io/nqa/gamedev/configuration/CORSConfiguration.java
@@ -0,0 +1,26 @@
+package io.nqa.gamedev.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfiguration {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "PUT", "POST", "UPDATE", "HEAD", "DELETE")
+                        .allowedHeaders("*")
+                        .exposedHeaders("*")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
