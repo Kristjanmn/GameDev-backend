849a725da1cc2f88ab3306ea25cab2b2b53ab952
Kristjanmn <kristjanmn@hotmail.com>
2022-06-17 02:48:28 +0300

    Added 'getById', 'getByDialogId' and 'getByProject' functions in DialogService

--- src/main/java/io/nqa/gamedev/service/DialogService.java
+++ src/main/java/io/nqa/gamedev/service/DialogService.java
@@ -1,7 +1,53 @@
 package io.nqa.gamedev.service;
 
+import io.nqa.gamedev.entity.Dialog;
+import io.nqa.gamedev.entity.Project;
+import io.nqa.gamedev.model.CustomResponse;
+import io.nqa.gamedev.repository.DialogRepository;
+import io.nqa.gamedev.service.global.GlobalService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class DialogService implements IDialogService {
+
+    @Autowired
+    private DialogRepository dialogRepository;
+
+    @Autowired
+    private IProjectService projectService;
+
+    @Override
+    public CustomResponse getById(String databaseId) {
+        if (GlobalService.isBlank(databaseId))
+            return new CustomResponse("Invalid database ID", null);
+        Optional<Dialog> optDialog = this.dialogRepository.findById(databaseId);
+        if (optDialog.isEmpty())
+            return new CustomResponse("Could not find dialog", null);
+        return new CustomResponse(optDialog.get());
+    }
+
+    @Override
+    public CustomResponse getByDialogId(String dialogId) {
+        if (GlobalService.isBlank(dialogId))
+            return new CustomResponse("Invalid ID", null);
+        Optional<Dialog> optDialog = this.dialogRepository.findByDialogIdEquals(dialogId);
+        if (optDialog.isEmpty())
+            return new CustomResponse("Could not find dialog", null);
+        return new CustomResponse(optDialog.get());
+    }
+
+    @Override
+    public CustomResponse getByProject(String projectDatabaseId) {
+        Project project = this.projectService.getById(projectDatabaseId);
+        if (GlobalService.isNull(project))
+            return new CustomResponse("Invalid project database ID", null);
+        List<Dialog> dialogs = project.getDialogs();
+        if (GlobalService.isNull(dialogs))
+            return new CustomResponse("Project exists, but dialogs was null", null);
+        return new CustomResponse(dialogs);
+    }
 }
--- src/main/java/io/nqa/gamedev/service/IDialogService.java
+++ src/main/java/io/nqa/gamedev/service/IDialogService.java
@@ -1,4 +1,12 @@
 package io.nqa.gamedev.service;
 
+import io.nqa.gamedev.model.CustomResponse;
+
 public interface IDialogService {
+
+    CustomResponse getById(String databaseId);
+
+    CustomResponse getByDialogId(String dialogId);
+
+    CustomResponse getByProject(String projectDatabaseId);
 }
