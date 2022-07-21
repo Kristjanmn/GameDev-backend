3a421d3f25fb9a026e3a6e9126785fcae04160a5
Kristjanmn <kristjanmn@hotmail.com>
2022-07-21 12:14:17 +0300

    Removed commented out variables in ScriptVariable

--- src/main/java/io/nqa/gamedev/entity/ScriptVariable.java
+++ src/main/java/io/nqa/gamedev/entity/ScriptVariable.java
@@ -6,7 +6,6 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
-import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Data
@@ -19,9 +18,6 @@ public class ScriptVariable {
     @Id
     private String id;
 
-    //private int listOrder;
-
-    //@ManyToOne VariableType
     private String variableType;
 
     private String variableName;
